f7c3c407e47ec6a150efcc78cc18c2bd6449f1a3
laboriloresyilian-code <laboriloresyilian@gmail.com>
2026-09-10 16:12:25 -0400

    Cambiar base de datos a SQLite para PythonAnywhere

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,10 +65,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # BASE DE DATOS
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/tu_base_de_datos_local',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # VALIDACIÓN DE CONTRASEÑAS
